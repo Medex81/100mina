179864b1f2001d74d65128ced0b5c1bc4dde4700
Sergey Nikitin <medex81@vk.com>
2023-11-15 18:59:54 +0300

    Now you can't start exporting without highlighting the lesson.

--- app/scenes/toolbox.gd
+++ app/scenes/toolbox.gd
@@ -1,8 +1,17 @@
 extends VBoxContainer
 
+var _lesson:String
+
 
 func _on_export_pressed():
-	$file_export.visible = true
+	if not _lesson.is_empty():
+		$file_export.visible = true
+	else:
+		OS.alert(tr("key_error_select_lesson"), tr("key_title_error"))
 
 func _on_import_pressed():
 	$file_import.visible = true
+
+
+func _on_lessons_send_lesson_clicked(lesson):
+	_lesson = lesson
