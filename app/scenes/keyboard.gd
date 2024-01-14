e00327c3107a56485dba604e68e6584b73b845dd
Sergey Nikitin <medex81@vk.com>
2024-01-15 01:27:11 +0300

    The keyboard initialization sequence error has been fixed.

--- app/scenes/keyboard.gd
+++ app/scenes/keyboard.gd
@@ -11,12 +11,11 @@ var symbols_button_group:ButtonGroup
 var _key_binds:Dictionary
 # data from the lesson scene transmitted through the data mediator to engine.gd
 var _scene_data:KeyboardDataResource
-# crutch. If the input sent us a symbol before the initialization of the current node, we will save the symbol and process it when ready.
-var _first_symbol:String
 var _keyboard_dict:Dictionary
 
 # the signal to the fingers is highlighted in color
 signal send_select_finger(index:int)
+signal send_init_done()
 
 # interception of input - if a special button is pressed on the keyboard, then we will display the values on the buttons with modifiers
 func _unhandled_input(event):
@@ -34,18 +33,18 @@ func _ready():
 		$stop_type.visible = !_scene_data.edit_mode
 		_keyboard_dict = TypeEngine.load_keyboard(_scene_data.lang)
 		_key_binds = _keyboard_dict.get(TypeEngine.key_keys, {})
+		if _key_binds.is_empty():
+			OS.alert(tr("key_error_keyboard_version").format([_scene_data.lang]), tr("key_title_error"))
 	symbols_button_group = ResourceLoader.load(symbols_button_path)
 	specials_button_group = ResourceLoader.load(specials_button_path)
 	symbols_button_group.pressed.connect(on_button_click)
 	for button in symbols_button_group.get_buttons():
 		button.set_opt(_key_binds.get(button.name, {}))
-	
-	if not _first_symbol.is_empty():
-		_on_type_area_send_next_symbol(_first_symbol)
 		
 	if _scene_data and _scene_data.edit_mode:
 		for button in specials_button_group.get_buttons():
 			button.disabled = true
+	emit_signal("send_init_done")
 
 	
 func on_button_click(button:KeyButton):
@@ -56,23 +55,23 @@ func on_button_click(button:KeyButton):
 
 # select the button by the next character that the user must enter
 func _on_type_area_send_next_symbol(symbol):
-	if symbols_button_group:
-		for symbol_button in symbols_button_group.get_buttons():
-			if symbol_button.on_send_group(symbol):
-				emit_signal("send_select_finger", -1)
-				emit_signal("send_select_finger", symbol_button.finger)
-				if specials_button_group:
-					var button = specials_button_group.get_pressed_button()
-					if button:
-						button.button_pressed = false
-					
-					for spec_button in specials_button_group.get_buttons():
-						if spec_button.on_send_group(symbol_button.key_sets.get(symbol, ""), symbol_button.right_modif):
-							emit_signal("send_select_finger", spec_button.finger)
-							break
-				break
-	else:
-		_first_symbol = symbol
+	if symbols_button_group == null:
+		await self.send_init_done
+		
+	for symbol_button in symbols_button_group.get_buttons():
+		if symbol_button.on_send_group(symbol):
+			emit_signal("send_select_finger", -1)
+			emit_signal("send_select_finger", symbol_button.finger)
+			if specials_button_group:
+				var button = specials_button_group.get_pressed_button()
+				if button:
+					button.button_pressed = false
+				
+				for spec_button in specials_button_group.get_buttons():
+					if spec_button.on_send_group(symbol_button.key_sets.get(symbol, ""), symbol_button.right_modif):
+						emit_signal("send_select_finger", spec_button.finger)
+						break
+			break
 
 func _on_key_value_send_key_value_dict(dict:Dictionary):
 	# there is a dedicated button
