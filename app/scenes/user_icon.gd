2c7a9cb089f49817b6954f9220d2675e8ea56fbc
Nikitin Sergey <medex81@vk.com>
2024-06-05 18:11:07 +0300

    Add circle shader for a user icon.

--- app/scenes/user_icon.gd
+++ app/scenes/user_icon.gd
@@ -20,13 +20,12 @@ func _on_edit_icon_pressed():
 	$user_image_dlg.show()
 	
 func _on_cancel_pressed():
-	TypeEngine.set_user_icon(TypeEngine.get_current_user(), "")
+	TypeEngine.set_user_icon(TypeEngine.get_current_user())
 	$icon.texture = null
 	emit_signal("send_change_icon")
 
-func _on_user_image_dlg_file_selected(path:String):
-	var icon_path = TypeEngine.save_to_user_dir(path, TypeEngine.icons_path, TypeEngine.get_current_user())
-	if not icon_path.is_empty():
-		TypeEngine.set_user_icon(TypeEngine.get_current_user() ,icon_path.get_file())
-		$icon.texture = TypeEngine.get_user_icon(TypeEngine.get_current_user())
-		emit_signal("send_change_icon")
+func _on_user_image_dlg_file_selected(path:String)->bool:
+	TypeEngine.set_current_user_icon(path)
+	$icon.texture = TypeEngine.get_current_user_icon()
+	emit_signal("send_change_icon")
+	return true if $icon.texture else false
